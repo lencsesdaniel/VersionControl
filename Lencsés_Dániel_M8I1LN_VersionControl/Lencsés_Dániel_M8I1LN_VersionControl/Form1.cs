0807e7082d223f885d90ba0dd5454a36aa0d56da
Dániel Lencsés <daniel.lencses@stud.uni-corvinus.hu>
2020-09-27 22:40:27 +0200

    Adding

--- Lencsés_Dániel_M8I1LN_VersionControl/Lencsés_Dániel_M8I1LN_VersionControl/Form1.cs
+++ Lencsés_Dániel_M8I1LN_VersionControl/Lencsés_Dániel_M8I1LN_VersionControl/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lencsés_Dániel_M8I1LN_VersionControl.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,14 +11,28 @@ using System.Windows.Forms;
 
 namespace Lencsés_Dániel_M8I1LN_VersionControl
 {
+    
+
     public partial class Form1 : Form
     {
+        BindingList<Entities.User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             label1.Text = Resource1.LastName;
             label2.Text = Resource1.FirstName; 
-            button1.Text = Resource1.Add; 
+            button1.Text = Resource1.Add;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
