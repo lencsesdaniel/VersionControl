a5cea1662c4b88f95840105ed32a5aa8fe7cc3bb
Dániel Lencsés <daniel.lencses@stud.uni-corvinus.hu>
2020-09-27 21:51:49 +0200

    Initial commit

---
+++ Lencsés_Dániel_M8I1LN_VersionControl/Lencsés_Dániel_M8I1LN_VersionControl/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lencsés_Dániel_M8I1LN_VersionControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
