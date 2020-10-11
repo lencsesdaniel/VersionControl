7944cfc114d974417015ffb82c458dec28d10bbf
Dániel Lencsés <daniel.lencses@stud.uni-corvinus.hu>
2020-10-11 23:16:28 +0200

    task 3 and 4

--- Var_Lencsés_Dániel/Var_Lencsés_Dániel/Form1.cs
+++ Var_Lencsés_Dániel/Var_Lencsés_Dániel/Form1.cs
@@ -12,9 +12,13 @@ namespace Var_Lencsés_Dániel
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
