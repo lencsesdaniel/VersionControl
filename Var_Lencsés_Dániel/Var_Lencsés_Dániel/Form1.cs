3ef8dc3bbf37c843fa8bba628116d8aa09cb8ac3
Dániel Lencsés <daniel.lencses@stud.uni-corvinus.hu>
2020-10-11 23:22:15 +0200

    task5

--- Var_Lencsés_Dániel/Var_Lencsés_Dániel/Form1.cs
+++ Var_Lencsés_Dániel/Var_Lencsés_Dániel/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Var_Lencsés_Dániel.Entities;
 
 namespace Var_Lencsés_Dániel
 {
@@ -14,11 +15,20 @@ namespace Var_Lencsés_Dániel
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+        }
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
