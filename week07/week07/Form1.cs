4f5e9ea9610ea5c190a5a2a4f0db0dca1621e5c4
Dániel Lencsés <daniel.lencses@stud.uni-corvinus.hu>
2020-10-18 20:35:42 +0200

    task03

--- week07/week07/Form1.cs
+++ week07/week07/Form1.cs
@@ -7,14 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week07.MnbServiceReference;
 
 namespace week07
 {
     public partial class Form1 : Form
-    {
+    {  
         public Form1()
         {
             InitializeComponent();
+            task03();
+        }
+        public string result;
+        void task03()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            result = response.GetExchangeRatesResult;
         }
     }
 }
