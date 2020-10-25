405557e902737704102e8fac75223032042c384d
Dániel Lencsés <daniel.lencses@stud.uni-corvinus.hu>
2020-10-25 21:34:50 +0100

    Task5

--- week07_/week07_/Form1.cs
+++ week07_/week07_/Form1.cs
@@ -14,6 +14,7 @@ namespace week07_
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random(1234);
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
