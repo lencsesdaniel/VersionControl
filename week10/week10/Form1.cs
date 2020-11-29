fb9588d3cecfb9e531192bf91c614f86c3b72c3b
Dániel Lencsés <daniel.lencses@stud.uni-corvinus.hu>
2020-11-29 23:16:03 +0100

    Task06

--- week10/week10/Form1.cs
+++ week10/week10/Form1.cs
@@ -19,6 +19,7 @@ namespace week10
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
+        
         public Form1()
         {
             InitializeComponent();
@@ -31,10 +32,7 @@ namespace week10
             }
 
             gc.Start();
-            var playerList = from p in gc.GetCurrentPlayers()
-                             orderby p.GetFitness() descending
-                             select p;
-            var topPerformers = playerList.Take(populationSize / 2).ToList();
+            
         }
         private void Gc_GameOver(object sender)
         {
@@ -42,6 +40,25 @@ namespace week10
             label1.Text = string.Format(
                 "{0}. generáció",
                 generation);
+            gc.ResetCurrentLevel();
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
     }
 }
